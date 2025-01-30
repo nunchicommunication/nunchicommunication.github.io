1a24ee6a41b0b13e744b861ce73fd384d4f4ef0c
robertovicario <robertovicario.com@gmail.com>
2025-01-30 11:05:54 +0100

    updates

---
+++ app/public/wp-content/plugins/stackable-ultimate-gutenberg-blocks/src/compatibility/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once( plugin_dir_path( __FILE__ ) . './neve/index.php' );
+require_once( plugin_dir_path( __FILE__ ) . './woocommerce.php' );
